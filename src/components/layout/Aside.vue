85280d0a294971b85a4a2496029db0990b3bbe06
surmon-china <surmon@foxmail.com>
2016-10-15 23:31:58 +0800

    Building

--- src/components/layout/Aside.vue
+++ src/components/layout/Aside.vue
@@ -59,27 +59,193 @@
         </li>
       </ul>
     </div>
+    <div class="aside-ad">
+      <router-link to="http://s.click.taobao.com/ZaXp1Rx" target="_blank" class="ad-box">
+        <img src="http://i1.piimg.com/567571/184e5b1a3e1613f5.jpg">
+      </router-link>
+    </div>
     <div class="aside-calendar">
-      我是日历
+      <calendar></calendar>
     </div>
     <div class="aside-ad">
-      我是广告
+      <router-link to="http://s.click.taobao.com/ZaXp1Rx" target="_blank" class="ad-box">
+        <img src="http://p1.bpimg.com/567571/e85fb6270effc4c7.jpg">
+      </router-link>
     </div>
     <div class="aside-tag">
       <ul class="aside-tag-list">
-        <li class="list-item">
-          <i class="index"></i>
-          <router-link to="/tag/python3" class="title">python3</router-link>
-        </li>
+        <router-link to="/tag/python3" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-python"></i>
+            <span>Python3</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/angular2" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-socialangular"></i>
+            <span>Angular2</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/css3" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-css3"></i>
+            <span>css3</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/php" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-php"></i>
+            <span>php</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/nodejs" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-socialnodejs"></i>
+            <span>nodejs</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/JavaScript" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-js"></i>
+            <span>JavaScript</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/chrome" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-chrome"></i>
+            <span>chrome</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/linux" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-linux"></i>
+            <span>linux</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/stackoverflow" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-stackoverflow"></i>
+            <span>stackoverflow</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/iconsf" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-iconsf"></i>
+            <span>chrome</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/ubuntu" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-ubuntu"></i>
+            <span>ubuntu</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/windows" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-windows"></i>
+            <span>windows</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/html5" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-html5"></i>
+            <span>html5</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/vr" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-vr"></i>
+            <span>vr</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/http" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-http"></i>
+            <span>http</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/github" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-github"></i>
+            <span>github</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/mac" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-mac"></i>
+            <span>mac</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/wechat" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-wechat"></i>
+            <span>wechat</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/google" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-google2015logo"></i>
+            <span>google</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/databasesql" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-databasesql"></i>
+            <span>databasesql</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/redis" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-redis"></i>
+            <span>redis</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/jquery" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-jquery"></i>
+            <span>jquery</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/wordpress" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-wordpress"></i>
+            <span>wordpress</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/zhihu" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-zhihu"></i>
+            <span>知乎</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/douban" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-douban"></i>
+            <span>豆瓣</span>
+          </a>
+        </router-link>
+        <router-link to="/tag/internet" tag="li" class="list-item">
+          <a class="title">
+            <i class="iconfont icon-internet"></i>
+            <span>其他</span>
+          </a>
+        </router-link>
       </ul>
     </div>
   </aside>
 </template>
 
 <script>
-export default {
-  name: 'aside'
-}
+
+  import Calendar from './Calendar.vue'
+
+  export default {
+    name: 'aside',
+
+    components: {
+      Calendar
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -149,7 +315,7 @@ export default {
         height: 3em;
         line-height: 3em;
         margin: 0;
-        padding: 0 1em;
+        padding: 0 .8em;
         border-bottom: 1px dashed #eee;
 
         .iconfont {
@@ -166,7 +332,7 @@ export default {
           display: block;
           height: 1.9em;
           line-height: 1.9em;
-          padding: 0 1em;
+          padding: 0 .8em;
           margin-bottom: .5em;
           color: #333;
           @include text-overflow();
@@ -204,18 +370,56 @@ export default {
     }
 
     .aside-calendar {
-      padding: 1em;
+      padding: .8em;
       margin-bottom: 1em;
     }
 
     .aside-ad {
-      padding: 1em;
+      padding: .8em;
       margin-bottom: 1em;
+
+      > .ad-box {
+        opacity: .8;
+
+        img {
+          max-width: 100%;
+          border-top: 1px solid #1c4767;
+        }
+      }
     }
 
     .aside-tag {
-      padding: 1em;
+      padding: .8em;
+      padding-bottom: 0;
       margin-bottom: 1em;
+
+      .aside-tag-list {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        overflow: hidden;
+
+        .list-item {
+          display: inline-block;
+          float: left;
+          margin-right: .8em;
+          margin-bottom: .8em;
+          height: 2em;
+          line-height: 2em;
+          text-transform: capitalize;
+          background-color: $module-hover-bg;
+
+          &:hover {
+            background-color: darken($module-hover-bg, 40%);
+          }
+
+          .title {
+            display: block;
+            padding: 0 .8em;
+            font-family: CenturyGothic, -apple-system, BlinkMacSystemFont, "PingFang SC", "Helvetica Neue", "Hiragino Sans GB", "Segoe UI", "Microsoft YaHei", sans-serif;
+          }
+        }
+      }
     }
   }
 </style>
