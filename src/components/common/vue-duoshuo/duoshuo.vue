cc7863a4b0c94828718e0b29a05045fca73a72f8
surmon-china <surmon@foxmail.com>
2016-10-18 23:28:20 +0800

    Build duoshuo comment system

---
+++ src/components/common/vue-duoshuo/duoshuo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="vue-duoshuo">
+    <div class="ds-thread" data-thread-key="123" data-title="我是留言板" data-url="http://localhost:8080/guestbook"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'vue-duoshuo',
+  props: ['dataThreadKey', 'dataTitle', 'dataUrl'],
+  data: function() {
+    return {
+    }
+  },
+  mounted: function() {
+    console.log(this)
+  }
+}
+</script>
+
+<style>
+</style>
