4c302cd08e7092563690d28c8a7899b5dbc2d7f6
unknown <>
2016-11-30 22:30:22 +0800

    Update Nothing

--- src/api/data/article.js
+++ src/api/data/article.js
@@ -2,7 +2,7 @@ module.exports = {
   code: 1,
   data: {
     title: 'Javascript DI！Angular依赖注入的实现原理',
-    thumb: 'http://surmon.me/wp-content/themes/Surmon/timthumb.php?src=http://surmon.me/wp-content/uploads/2015/09/2329141422-0.jpg&h=180&w=300&q=90&zc=1',
+    thumb: 'http://wordpress.surmon.me/wp-content/themes/Surmon/timthumb.php?src=http://wordpress.surmon.me/wp-content/uploads/2015/09/2329141422-0.jpg&h=180&w=300&q=90&zc=1',
     description: '前言 一个多月前，卤煮读了一篇翻译过来的外国人写的技术博客。此君在博客中将js中的类（构造）继承和原型继承做了一些比较，并且得出了结论：建议诸位在开发是用原型继承。文中提到了各种原型继承的优点，详细的露珠不一一说明介绍了。这篇文章的名字是为什么原型继承很重要，有兴趣的同学可以去看，此文有些深度，值得反复阅读。今天这篇文章也来谈谈js中的继承方式以及它们的优缺点。 类式继承（构造函数） ',
     content: `<p>DI是Angular的特色功能，而在Angular 2.0的计划中，DI将成为一个独立的模块，参见 https://github.com/angular/di.js 这意味着它也有机会被用于nodejs等技术中，其他前端框架也完全有机会使用它。</p>
       <h3 id="h3-0">DI简介</h3>
