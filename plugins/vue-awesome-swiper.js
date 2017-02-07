ebacb2c845cb1efabdeb874691413229b25e2408
surmon <surmon@foxmail.com>
2017-02-07 21:46:26 +0800

    article releted tag builded success

--- plugins/vue-awesome-swiper.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-if (process.BROWSER_BUILD) {
-	let { swiper, swiperSlide } = require('~components/common/vue-awesome-swiper')
-  Vue.component('swiper', swiper)
-  Vue.component('swiper-slide', swiperSlide)
-}
