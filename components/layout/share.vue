9e36bfe6bba5697712bc4297f3d117723dc543af
surmon <surmon@foxmail.com>
2017-02-14 23:32:44 +0800

    add share func

---
+++ components/layout/share.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="share">
+    23234
+    <div class="share-box">
+<!--       <div class="a2a_kit a2a_kit_size_32 a2a_default_style sidebar-share">
+        <a class="a2a_button_wechat"></a>
+        <a class="a2a_button_sina_weibo"></a>
+        <a class="a2a_button_qzone"></a>
+        <a class="a2a_button_douban"></a>
+        <a class="a2a_button_renren"></a>
+        <a class="a2a_button_evernote"></a>
+        <a class="a2a_button_facebook"></a>
+        <a class="a2a_button_twitter"></a>
+        <a class="a2a_button_google_plus"></a>
+        <a class="a2a_button_pinterest"></a>
+        <a class="a2a_button_wordpress"></a>
+        <a class="a2a_button_linkedin"></a>
+        <a class="a2a_button_email"></a>
+        <a class="a2a_button_copy_link"></a>
+        <a class="a2a_dd" href="https://www.addtoany.com/share?linkurl=https%3A%2F%2Fsurmon.me&amp;linkname=Surmon.me"></a>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'share',
+    methods: {
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '~assets/sass/variables';
+  #share {
+    position: fixed;
+    width: 100%;
+    bottom: 20em;
+  }
+</style>
