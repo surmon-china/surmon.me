2237edfe8454e4e32653e875b56ab035cde6e4b4
surmon-china <surmon@foxmail.com>
2017-02-15 12:37:42 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ module.exports = {
   plugins: [
     '~plugins/ga.js',
     '~plugins/axios.js',
-    '~plugins/share.js',
+    // '~plugins/share.js',
     '~plugins/cheerio.js',
     '~plugins/clipboard.js',
     '~plugins/highlight.js',
