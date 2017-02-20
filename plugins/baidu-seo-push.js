22e575b40c27e153ee7b65514fce63e13d069d34
surmon-china <surmon@foxmail.com>
2017-02-20 21:31:09 +0800

    add baidu seo auto push

---
+++ plugins/baidu-seo-push.js
@@ -0,0 +1,34 @@
+/*
+** 只在生成模式的客户端中使用
+*/
+
+if (process.BROWSER_BUILD && process.env.NODE_ENV === 'production') {
+  /*
+  ** 百度seo-自动push脚本
+  ** https://zz.bdstatic.com/linksubmit/push.js
+  */
+  const baiduPush = href => {
+  	!function(){
+	    let e = /([http|https]:\/\/[a-zA-Z0-9\_\.]+\.baidu\.com)/gi
+	    let r = href || window.location.href
+	    let t = document.referrer
+	    if (!e.test(r)) {
+	      let o = "https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif"
+	      t ? (o += "?r=" + encodeURIComponent(document.referrer), r && (o += "&l=" + r)) : r && (o += "?l=" + r)
+	      let i = new Image;
+	      i.src = o
+	    }
+		}(window)
+  }
+
+  baiduPush()
+
+  /*
+  ** 应用挂载后
+  */
+  window.onNuxtReady((app) => {
+    app.$nuxt.$on('routeChanged', (to, from) => {
+      baiduPush(window.location.origin + to.fullPath)
+    })
+  })
+}
