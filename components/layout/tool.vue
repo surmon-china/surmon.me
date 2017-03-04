fff6c0d8c431c395faa4edcefd92371bd703e7c1
surmon-china <surmon@foxmail.com>
2017-03-05 03:32:49 +0800

    commit 模块初步完成

--- components/layout/tool.vue
+++ components/layout/tool.vue
@@ -20,26 +20,15 @@
     name: 'tool',
     methods: {
       totop(acceleration = 0.1, stime = 10) {
-        let [x1, y1, x2, y2, x3, y3] = [0, 0, 0, 0, 0, 0]
-        if (document.documentElement) {
-          x1 = document.documentElement.scrollLeft || 0
-          y1 = document.documentElement.scrollTop || 0
-        }
-        if (document.body) {
-          x2 = document.body.scrollLeft || 0
-          y2 = document.body.scrollTop || 0
-        }
-        x3 = window.scrollX || 0
-        y3 = window.scrollY || 0
-        // 滚动条到页面顶部的水平距离
-        let x = Math.max(x1, Math.max(x2, x3))
-        // 滚动条到页面顶部的垂直距离
-        let y = Math.max(y1, Math.max(y2, y3))
+        let currentScrollTop = Math.max.apply(Math, [
+          window.scrollY || 0, 
+          document.body.scrollTop || 0, 
+          document.documentElement.scrollTop || 0
+        ])
         // 滚动距离 = 目前距离 / 速度, 因为距离原来越小, 速度是大于 1 的数, 所以滚动距离会越来越小
-        let speeding = 1 + acceleration
-        window.scrollTo(Math.floor(x / speeding), Math.floor(y / speeding))
-        // 如果距离不为零, 继续调用函数
-        if(x > 0 || y > 0) {
+        window.scrollTo(0, Math.floor(currentScrollTop / (1 + acceleration)))
+        // 如果距离还没到
+        if(currentScrollTop > 0) {
           window.setTimeout(() => {
             this.totop(acceleration, stime)
           }, stime)
