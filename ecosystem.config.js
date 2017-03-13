b8631366faa1822a06c2c1c83fd618a2e016245e
surmon-china <surmon@foxmail.com>
2017-03-14 00:29:31 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "surmon.me",
       script: "server.js",
       args: "cross-env",
-      // max_memory_restart: "198M",
+      max_memory_restart: "198M",
       log_date_format: "YYYY-MM-DD HH:mm Z",
       error_file: "/usr/local/wwwlogs/surmon.me/error.log",
       out_file: "/usr/local/wwwlogs/surmon.me/out.log",
