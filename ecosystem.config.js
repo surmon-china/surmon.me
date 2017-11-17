d428fb4ffd434da15472bb3c03f0335a1600d106
surmon <mcc@meitu.com>
2017-11-17 14:45:57 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,10 @@ module.exports = {
       log_date_format: "YYYY-MM-DD HH:mm Z",
       error_file: "/usr/local/wwwlogs/surmon.me/error.log",
       out_file: "/usr/local/wwwlogs/surmon.me/out.log",
+      args: [
+        "--rtc_uri=turn:47.96.19.254:3478",
+        "--rtc_key=46c51c9b608743bdbd60eabe1424bc5f"
+      ],
       env: {
         "NODE_ENV": "production",
       }
