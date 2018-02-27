336b788509fea208ac31eaef607647b148598727
surmon-china <surmon.me@gmail.com>
2018-02-27 14:01:44 +0800

    optimize 404 page and update nuxt

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,10 +3,11 @@
     <div class="error-content">
       <h1 class="error-code">{{ error.statusCode }}</h1>
       <div class="error-wrapper-message">
-        <h2 class="error-message">{{ error.message || '众里寻他 我已不再' }}</h2>
+        <!-- <h2 class="error-message">{{ error.message || '众里寻他 我已不再' }}</h2> -->
+        <h2 class="error-message">众里寻他 我已不再</h2>
       </div>
-      <p>
-        <nuxt-link class="error-link" to="/">Back to the home page</nuxt-link>
+      <p class="error-link">
+        <nuxt-link class="link" to="/">Back to the home page</nuxt-link>
       </p>
     </div>
   </div>
@@ -25,22 +26,52 @@
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   @import '~assets/sass/mixins';
   @import '~assets/sass/variables';
   .error {
-    position: relative;
-    background-color: $module-bg;
-    min-height: 10em;
+    top: 0;
+    left: 0;
     width: 100%;
+    height: 100%;
+    position: fixed;
     overflow: hidden;
+    z-index: 999;
     text-align: center;
-    padding: 2em 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(255, 255, 255, .6);
 
-    .error-content {
+    @keyframes error-item {
+      0% {
+        opacity: 0;
+        transform: translate3d(0, -30%, 0);
+      }
+      100% {
+        opacity: 1;
+        transform: translate3d(0, 0, 0);
+      }
+    }
 
-      .error-code {
-        font-size: 9rem;
+    > .error-content {
+
+      > .error-code,
+      > .error-wrapper-message,
+      > .error-link {
+        animation: error-item ease-out both .5s .3s;
+      }
+
+      > .error-code {
+        font-size: 12rem;
+        margin: 0;
+      }
+
+      > .error-wrapper-message {
+
+        > .error-message {
+          margin-top: 0;
+        }
       }
     }
   }
