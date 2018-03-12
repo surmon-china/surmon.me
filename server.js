550ae006c378e5af74293e7517fd29693bd2f5ec
surmon-china <surmon@foxmail.com>
2018-03-12 16:14:42 +0800

    部署 doDeploy

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server, {
-  // serveClient: false,
   transports: ['websocket']
 })
 const { Nuxt, Builder } = require('nuxt')
