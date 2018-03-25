435c2b6f191ad94b71f43513f7fa371832b695db
surmon-china <surmon@foxmail.com>
2018-03-25 17:30:26 +0800

    忘记部署了 doDeploy

--- api.config.js
+++ api.config.js
@@ -4,5 +4,5 @@ const isProdMode = Object.is(process.env.NODE_ENV, 'production')
 module.exports = {
   cdnUrl: isProdMode ? 'https://cdn.surmon.me' : '',
   baseUrl: isProdMode ? 'https://api.surmon.me/' : 'http://localhost:8000/',
-  socketHost: isProdMode ? 'https://surmon.me' : `http://localhost:3000`
+  socketHost: isProdMode ? 'https://surmon.me' : 'http://localhost:3000'
 }
