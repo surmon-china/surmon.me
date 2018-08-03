3aa764f34a2a31924feab6aab0e21ada28a86e01
surmon-china <surmon@foxmail.com>
2018-08-03 15:30:08 +0800

    暂时关闭 CDN

--- api.config.js
+++ api.config.js
@@ -2,7 +2,8 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-  cdnUrl: isProdMode ? 'https://cdn.surmon.me' : '',
+  // cdnUrl: isProdMode ? 'https://cdn.surmon.me' : '',
+  cdnUrl: isProdMode ? '' : '',
   baseUrl: isProdMode ? 'https://api.surmon.me/' : 'http://localhost:8000/',
   socketHost: isProdMode ? 'https://surmon.me' : 'http://localhost:3000'
 }
