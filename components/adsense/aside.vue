919257d885b8d642664ab322747abc753f608a52
surmon-china <surmon@foxmail.com>
2018-11-06 03:58:00 +0800

    optimize adsense style and doDeploy

--- components/adsense/aside.vue
+++ components/adsense/aside.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="adsense adsense-aside">
     <no-ssr>
-      <Adsense
+      <!-- <Adsense
         root-class="adsense-wrapper"
         data-ad-layout-key="-ho-m+39-79+5o"
         data-ad-client="ca-pub-4710915636313788"
         data-ad-slot="8929033492"
         data-full-width-responsive="true">
+      </Adsense> -->
+      <Adsense
+        root-class="adsense-wrapper"
+        data-ad-client="ca-pub-4710915636313788"
+        data-ad-slot="9192348255">
       </Adsense>
     </no-ssr>
   </div>
