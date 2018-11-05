0542b65ba453fb3e59f328d81f126cf756c93128
surmon-china <surmon@foxmail.com>
2018-11-06 04:41:20 +0800

    optimize adsense style and doDeploy

--- components/adsense/article.vue
+++ components/adsense/article.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="adsense adsense-article">
+    <no-ssr>
+      <InArticleAdsense
+        root-class="adsense-wrapper"
+        data-ad-client="ca-pub-4710915636313788"
+        data-ad-slot="7405264495">
+      </InArticleAdsense>
+    </no-ssr>
   </div>
 </template>
 
