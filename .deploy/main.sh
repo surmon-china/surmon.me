7a8a081e4031a205dcdc1ffc5db0e3e2442d35bd
surmon-china <surmon.me@gmail.com>
2019-10-07 08:44:11 +0800

    Update deploy script

--- .deploy/main.sh
+++ .deploy/main.sh
@@ -2,7 +2,8 @@
 
 SHELL_PATH=$(dirname $0)
 
+echo ${SHELL_PATH}/rebuild.sh
 echo "[deploy] Start deployment..."
-sh ./${SHELL_PATH}/rebuild.sh
-sh ./${SHELL_PATH}/restart.sh
+sh ${SHELL_PATH}/rebuild.sh
+sh ${SHELL_PATH}/restart.sh
 echo "[deploy] Finished."
