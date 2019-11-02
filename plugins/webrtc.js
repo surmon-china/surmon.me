7967f0b3f816dfa9fa9edeac7a61d33ce423956b
surmon-china <surmon@foxmail.com>
2019-11-03 03:53:05 +0800

    Fix WebRTC mobile bug and update ads

--- plugins/webrtc.js
+++ plugins/webrtc.js
@@ -9,44 +9,46 @@ import { isBrowser } from '~/environment'
 let SimpleWebRTC = null
 
 if (isBrowser) {
+  if (!window.__NUXT__.state.global.isMobile) {
     SimpleWebRTC = require('simplewebrtc')
     // https://github.com/andyet/SimpleWebRTC/blob/master/src/simplewebrtc.js#L327
     SimpleWebRTC.prototype.joinRoom = function (name, cb) {
-        var self = this;
-        this.roomName = name;
-        this.connection.emit('join', name, function (err, roomDescription) {
-            // console.log('join CB', err, roomDescription);
-            if (err) {
-                self.emit('error', err);
-            } else {
-                var id,
-                    client,
-                    type,
-                    peer;
-                for (id in roomDescription.clients) {
-                    client = roomDescription.clients[id];
-                    for (type in client) {
-                        if (client[type]) {
-                            peer = self.webrtc.createPeer({
-                                id: id,
-                                type: type,
-                                enableDataChannels: self.config.enableDataChannels && type !== 'screen',
-                                receiveMedia: {
-                                    offerToReceiveAudio: type !== 'screen' && self.config.receiveMedia.offerToReceiveAudio ? 1 : 0,
-                                    offerToReceiveVideo: self.config.receiveMedia.offerToReceiveVideo
-                                }
-                            });
-                            self.emit('createdPeer', peer);
-                            peer.start();
-                        }
-                    }
-                }
+      var self = this;
+      this.roomName = name;
+      this.connection.emit('join', name, function (err, roomDescription) {
+        // console.log('join CB', err, roomDescription);
+        if (err) {
+          self.emit('error', err);
+        } else {
+          var id,
+            client,
+            type,
+            peer;
+          for (id in roomDescription.clients) {
+            client = roomDescription.clients[id];
+            for (type in client) {
+              if (client[type]) {
+                peer = self.webrtc.createPeer({
+                  id: id,
+                  type: type,
+                  enableDataChannels: self.config.enableDataChannels && type !== 'screen',
+                  receiveMedia: {
+                    offerToReceiveAudio: type !== 'screen' && self.config.receiveMedia.offerToReceiveAudio ? 1 : 0,
+                    offerToReceiveVideo: self.config.receiveMedia.offerToReceiveVideo
+                  }
+                });
+                self.emit('createdPeer', peer);
+                peer.start();
+              }
             }
-
-            if (cb) cb(err, roomDescription);
-            self.emit('joinedRoom', name);
-        });
+          }
+        }
+  
+        if (cb) cb(err, roomDescription);
+        self.emit('joinedRoom', name);
+      });
     };
+  }
 }
 
 export default SimpleWebRTC
