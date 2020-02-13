24d7fc17028fbaf13d9715bc4e0bfa45efd92639
surmon-china <surmon.me@gmail.com>
2020-02-14 01:06:42 +0800

    Update GA

--- components/common/adsense/archive-mobile.vue
+++ components/common/adsense/archive-mobile.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="mammon-box archive mobile">
-    <client-only>
-      <InFeedAdsense
-        root-class="mammon-wrapper"
-        data-ad-layout-key="-hw-7+2w-11-86"
-        data-ad-client="ca-pub-4710915636313788"
-        data-ad-slot="6538975194"
-      />
-    </client-only>
-  </div>
+  <client-only>
+    <InFeedAdsense
+      root-class="mammon-box"
+      ins-class="mammon-ins"
+      data-ad-layout-key="-hw-7+2w-11-86"
+      data-ad-client="ca-pub-4710915636313788"
+      data-ad-slot="6538975194"
+    />
+  </client-only>
 </template>
 
 <script>
