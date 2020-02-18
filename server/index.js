81ca9db68b6dd2d0413891d5366bf6398e196dad
surmon-china <surmon.me@gmail.com>
2020-02-18 13:55:16 +0800

    Upgrade

--- server/index.js
+++ server/index.js
@@ -29,7 +29,6 @@ const port = environment.PORT || 3000
 const host = environment.HOST || '127.0.0.1'
 
 // extends
-const handleRSS = require('./rss')
 const barrageServer = require('./barrage')
 const updateGAScript = require('./analytics')
 
@@ -51,7 +50,6 @@ if (config.dev) {
   app.get('/proxy/*', handleProxy())
 }
 
-app.get('/rss', handleRSS)
 app.use(nuxt.render)
 app.set('port', port)
 
