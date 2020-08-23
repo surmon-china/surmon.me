bdbd25aefdc51524b0e1925c43fe322775c94acb
surmon-china <surmon.me@gmail.com>
2020-08-24 01:36:17 +0800

    TODO: ad & namespace.css.su-container

--- src/components/adsense/archive-mobile.vue
+++ src/components/adsense/archive-mobile.vue
@@ -1,12 +1,10 @@
 <template>
-  <client-only>
-    <Adsense
-      ins-class="mammon-ins"
-      data-ad-format="fluid"
-      data-ad-layout-key="-hw-7+2w-11-86"
-      data-ad-slot="6538975194"
-    />
-  </client-only>
+  <Adsense
+    ins-class="mammon-ins"
+    data-ad-format="fluid"
+    data-ad-layout-key="-hw-7+2w-11-86"
+    data-ad-slot="6538975194"
+  />
 </template>
 
 <script>
