303f33d97ac12fdc902e95b1ba66cd6e1fffbf1f
surmon-china <surmon.me@gmail.com>
2020-12-02 22:00:25 +0800

    chore: add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+/**
+ * @file PM2 config
+ * @module config/pm2
+ * @author Surmon <surmon@foxmail.com>
+ */
+
+module.exports = {
+  apps: [
+    {
+      name: "surmon.me",
+      instances: "max",
+      exec_mode: "cluster",
+      script: "dist/server.js",
+      max_memory_restart: "268M",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "/usr/local/wwwlogs/surmon.me/error.log",
+      out_file: "/usr/local/wwwlogs/surmon.me/out.log",
+      env: {
+        "NODE_ENV": "production",
+      }
+    }
+  ]
+}
