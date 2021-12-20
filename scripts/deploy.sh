055027c666e02aa27f156c3ddf615b1ed5135aa6
surmon-china <surmon.me@gmail.com>
2021-12-21 05:47:16 +0800

    chore: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,6 @@ SHELL_PATH=$(dirname $0)
 
 echo "[deploy] start deployment..."
 sh ${SHELL_PATH}/refetch.sh
-
-# install dependencies & clone release resource
 sh ${SHELL_PATH}/reinstall.sh
 sh ${SHELL_PATH}/reclone.sh
 sh ${SHELL_PATH}/restart.sh
