4176ed514844655087a1098f2f812da0d75db02b
surmon-china <surmon.me@gmail.com>
2022-12-23 15:52:01 +0800

    ci: update deploy scripts

---
+++ scripts/reload.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+SHELL_PATH=$(dirname $0)
+
+cd $SHELL_PATH
+cd ..
+
+echo "[deploy] downloading..."
+rm -rf dist
+mkdir dist
+cd dist
+curl -OL https://github.com/surmon-china/surmon.me/archive/refs/heads/release.zip && tar -xzf release.zip
+mv surmon.me-release/* ./
+rm -rf surmon.me-release
+rm -rf release.zip
+echo "[deploy] download release done"
