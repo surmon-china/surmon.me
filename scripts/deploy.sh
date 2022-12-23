64dbc58d7cb0cceae6d1af71ea9a5eb8bb8c9c1c
surmon-china <surmon.me@gmail.com>
2022-12-23 15:32:47 +0800

    ci: updage desploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+APP_NAME=surmon.me
+
 SHELL_PATH=$(dirname $0)
 
 echo "[deploy] start deployment..."
+
+pm2 stop $APP_NAME
+
 sh ${SHELL_PATH}/refetch.sh
 sh ${SHELL_PATH}/reinstall.sh
 sh ${SHELL_PATH}/reclone.sh
 sh ${SHELL_PATH}/restart.sh
+
+pm2 start $APP_NAME
+
 echo "[deploy] finished"
--- scripts/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "[deploy] restarting..."
-pm2 restart surmon.me
-echo "[deploy] restart done"
