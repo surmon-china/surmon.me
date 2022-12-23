e40f20e062bb517828391de540efd0289a15241f
surmon-china <surmon.me@gmail.com>
2022-12-23 16:08:32 +0800

    ci: fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-APP_NAME=surmon.me
+PM2_APP_NAME=surmon.me
 
 SHELL_PATH=$(dirname $0)
 
 echo "[deploy] start deployment..."
 
-pm2 stop $APP_NAME
+pm2 stop $PM2_APP_NAME
 
 sh ${SHELL_PATH}/refetch.sh
 sh ${SHELL_PATH}/reinstall.sh
 sh ${SHELL_PATH}/reload.sh
 
-pm2 start $APP_NAME
+pm2 restart $PM2_APP_NAME
 
 echo "[deploy] finished"
