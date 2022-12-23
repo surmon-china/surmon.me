f49cd01b159976a66255b03d65acbdbb4793ca91
surmon-china <surmon.me@gmail.com>
2022-12-23 16:25:10 +0800

    ci: improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,15 @@ echo "[deploy] start deployment..."
 # pm2: not found
 # sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/pm2" "/usr/local/bin/pm2"
 echo "[deploy] stop pm2 app..."
-pm2 stop $PM2_APP_NAME
+# -s –silent: hide all messages
+# https://pm2.io/docs/runtime/reference/pm2-cli/
+pm2 stop $PM2_APP_NAME -s
 
 sh ${SHELL_PATH}/refetch.sh
 sh ${SHELL_PATH}/reinstall.sh
 sh ${SHELL_PATH}/reload.sh
 
 echo "[deploy] restart pm2 app..."
-pm2 restart $PM2_APP_NAME
+pm2 restart $PM2_APP_NAME -s
 
 echo "[deploy] finished"
