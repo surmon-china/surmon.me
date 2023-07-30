5b2f089e0c5c0ba73053e800fcfe5b0b4c61429e
Surmon <surmon.me@gmail.com>
2023-07-31 02:30:50 +0700

    feat: improve Creative Commons

--- src/pages/article/meta.vue
+++ src/pages/article/meta.vue
@@ -73,7 +73,7 @@
       <i18n>
         <template #zh>
           <ulink class="link copyright" href="https://creativecommons.org/licenses/by-nc/4.0/deed.zh">
-            自由转载 - 署名 - 非商业性使用
+            署名 - 非商业性使用 4.0 国际
           </ulink>
         </template>
         <template #en>
@@ -126,7 +126,6 @@
       .icon {
         font-weight: normal;
         font-size: $font-size-h6;
-        margin-right: $xs-gap;
       }
 
       .link {
