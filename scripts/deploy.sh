80bd9b5ec2454af0d03537df899c06d38da337ea
Surmon <surmon.me@gmail.com>
2023-11-19 18:32:27 +0800

    ci: deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
+# scripts directory
+SHELL_PATH=$(dirname "$0")
+
+# PM2 application name
 PM2_APP_NAME=surmon.me
 
-SHELL_PATH=$(dirname $0)
+echo "[deploy] start..."
 
-echo "[deploy] start deployment..."
-
-# pm2: not found
-# sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/pm2" "/usr/local/bin/pm2"
+# Stop the PM2 application
 echo "[deploy] stop pm2 app..."
-# -s –silent: hide all messages
-# https://pm2.io/docs/runtime/reference/pm2-cli/
-pm2 stop $PM2_APP_NAME -s
+# -s –silent: hide all messages https://pm2.io/docs/runtime/reference/pm2-cli/
+pm2 stop "$PM2_APP_NAME" -s
 
-sh ${SHELL_PATH}/refetch.sh
-sh ${SHELL_PATH}/reinstall.sh
-sh ${SHELL_PATH}/reload.sh
+# Navigate to the project root directory
+cd "$SHELL_PATH" || exit 1
+cd ..
 
+# Print information
+echo "[deploy] fetching..."
+echo "[deploy] pulling source code..."
+
+# Fetch the latest code
+git fetch --all && git reset --hard origin/main && git pull
+git checkout main
+
+# Install dependencies
+echo "[deploy] pnpm installing..."
+pnpm install --frozen-lockfile
+
+# Print information
+echo "[deploy] release downloading..."
+
+# Download and unzip the release package
+rm -rf dist
+mkdir -p dist
+cd dist || exit 1
+curl -sOL https://github.com/surmon-china/surmon.me/archive/refs/heads/release.zip && unzip -q release.zip
+mv surmon.me-release/* ./
+rm -rf surmon.me-release
+rm -rf release.zip
+
+# Restart the PM2 application
 echo "[deploy] restart pm2 app..."
-pm2 restart $PM2_APP_NAME -s
+pm2 restart "$PM2_APP_NAME" -s
 
 echo "[deploy] finished"
