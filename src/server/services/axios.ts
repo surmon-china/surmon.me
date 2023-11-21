4f658f94537ab8a4b97ad8b448e9641e75079292
Surmon <surmon.me@gmail.com>
2023-11-22 04:19:14 +0800

    fix(bff): axios https agent

--- src/server/services/axios.ts
+++ src/server/services/axios.ts
@@ -1,12 +1,26 @@
 import _axios from 'axios'
-import https from 'https'
+import Agent, { HttpsAgent } from 'agentkeepalive'
 
 // https://stackoverflow.com/a/63585550/6222535
+// https://stackoverflow.com/a/74375876/6222535
+// https://github.com/node-modules/agentkeepalive
 // https://github.com/axios/axios?tab=readme-ov-file#request-config
 // https://github.com/node-fetch/node-fetch/issues/1295
 const axios = _axios.create({
-  timeout: 60000,
-  httpsAgent: new https.Agent({ keepAlive: true })
+  httpAgent: new Agent({
+    maxSockets: 160,
+    maxFreeSockets: 160,
+    timeout: 60000,
+    freeSocketTimeout: 30000,
+    keepAliveMsecs: 60000
+  }),
+  httpsAgent: new HttpsAgent({
+    maxSockets: 160,
+    maxFreeSockets: 160,
+    timeout: 60000,
+    freeSocketTimeout: 30000,
+    keepAliveMsecs: 60000
+  })
 })
 
 export default axios
