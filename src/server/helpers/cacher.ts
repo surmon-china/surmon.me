43e04b2ab71000625ab8af65edd81c658854cbb2
Surmon <surmon.me@gmail.com>
2023-11-22 09:23:22 +0800

    feat(bff): improve cacher

--- src/server/helpers/cacher.ts
+++ src/server/helpers/cacher.ts
@@ -54,9 +54,9 @@ const passive = async <T = any>(cache: CacheClient, opts: CacherOptions<T>): Pro
   } catch (error) {
     logger.failure(
       'passive failure:',
-      `"${opts.key}" |`,
-      `ttl: ${humanizeSeconds(options.ttl)} |`,
-      `"${getErrorMessage(error)}"`
+      `"${opts.key}"`,
+      `| ttl: ${humanizeSeconds(options.ttl)}`,
+      `| "${getErrorMessage(error)}"`
     )
     return Promise.reject(error)
   }
@@ -64,7 +64,7 @@ const passive = async <T = any>(cache: CacheClient, opts: CacherOptions<T>): Pro
 
 // Unique ID for interval task lock
 const UNIQUE_INTERVAL_LOCK_ID = crypto.randomBytes(8).toString('hex')
-logger.debug('UNIQUE_INTERVAL_LOCK_ID:', UNIQUE_INTERVAL_LOCK_ID)
+// logger.debug('UNIQUE_INTERVAL_LOCK_ID:', UNIQUE_INTERVAL_LOCK_ID)
 
 export interface IntervalCacherOptions<T> extends CacherOptions<T> {
   interval: Seconds
@@ -75,49 +75,55 @@ const interval = <T>(cache: CacheClient, opts: IntervalCacherOptions<T>) => {
   const options = { ...opts, key: getCacheKey(opts.key) }
   const intervalLockKey = getLockCacheKey(opts.key)
 
-  // In normal mode: the only instance performs the interval task
-  // In cluster mode: only one instance performs the interval task at the same time
+  // In normal mode: the only instance performs the interval task.
+  // In cluster mode: only one instance performs the interval task at the same time.
   const execInterval = async (succeedCount = 0, failureCount = 0) => {
-    // 1. Acquire locks before executing tasks to determine execution privileges
+    // 1. Acquire locks before executing tasks to determine execution privileges.
     const lock = await cache.get<string>(intervalLockKey)
     const executable = !lock || lock === UNIQUE_INTERVAL_LOCK_ID
-    logger.debug('execInterval', { lock, executable, intervalLockKey })
+    // logger.debug('execInterval', { lock, executable, intervalLockKey })
 
     try {
-      // 2. Execute the task only in the (no lock) (self-locking) cases
+      // 2. Execute the task only in the (no lock) (self-locking) cases.
       if (executable) {
-        // Temporarily set permanent survival for locks to prevent them from expiring during task execution
+        // Temporarily set permanent survival for locks to prevent them from expiring during task execution.
         await cache.set(intervalLockKey, UNIQUE_INTERVAL_LOCK_ID)
         await execute(cache, options)
+        // Incrementing the value of the count
+        ++succeedCount
         logger.success(
           'interval succeed:',
-          `${failureCount}/${succeedCount + 1}`,
+          `${failureCount}/${succeedCount}`,
           `| "${opts.key}"`,
           `| ttl: ${humanizeSeconds(options.ttl)}`,
           `| next: ${humanizeSeconds(options.interval)}`
         )
-        // After the task succeeds, update the lock's lifecycle to be consistent with the interval of the task
-        await cache.set(intervalLockKey, UNIQUE_INTERVAL_LOCK_ID, options.interval)
+        // After the task succeeds, update the lock's lifecycle to be consistent with the interval of the task.
+        // Since JavaScript timeout is subject to uncertainty, allow some time for fault tolerance.
+        await cache.set(intervalLockKey, UNIQUE_INTERVAL_LOCK_ID, options.interval + 2)
       } else {
         // 3. If the current task is already occupied by another instance, skip it
-        logger.debug('interval skip:', `"${opts.key}"`, `| lock: "${lock}"`)
+        // logger.debug('interval skip:', `"${opts.key}"`, `| lock: "${lock}"`)
       }
 
       // Regardless of whether the task is actually performed or not,
-      // the timing check is performed again after an interval of time
-      setTimeout(() => execInterval(succeedCount + 1, failureCount), options.interval * 1000)
+      // the timing check is performed again after an interval of time.
+      // logger.debug('new interval:', `"${opts.key}"`, options.interval)
+      setTimeout(() => execInterval(succeedCount, failureCount), options.interval * 1000)
     } catch (error) {
+      // Incrementing the value of the count
+      ++failureCount
       logger.failure(
         'interval failure:',
-        `${failureCount + 1}/${succeedCount}`,
+        `${failureCount}/${succeedCount}`,
         `| "${opts.key}"`,
         `| retry: ${humanizeSeconds(options.retry)}`,
         `| "${getErrorMessage(error)}"`
       )
-      // If the task execution fails, the timing check will be executed again after the retry time
-      setTimeout(() => execInterval(succeedCount, failureCount + 1), options.retry * 1000)
-      // It should also update the lock's lifecycle to be consistent with the time of the retry
-      await cache.set(intervalLockKey, UNIQUE_INTERVAL_LOCK_ID, options.retry)
+      // If the task execution fails, the timing check will be executed again after the retry time.
+      setTimeout(() => execInterval(succeedCount, failureCount), options.retry * 1000)
+      // It should also update the lock's lifecycle to be consistent with the time of the retry.
+      await cache.set(intervalLockKey, UNIQUE_INTERVAL_LOCK_ID, options.retry + 2)
     }
   }
 
