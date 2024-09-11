596386e0fe41ec39c4e099c96a5055d19c42be62
Surmon <surmon.me@gmail.com>
2024-09-12 01:00:39 +0800

    feat: disabled `<link rel=modulepreload ...>`

--- sbundler/step-client.js
+++ sbundler/step-client.js
@@ -23,7 +23,12 @@ export const bundleClientRender = async (paths) => {
       sourcemap: true,
       minify: true,
       manifest: true,
-      emptyOutDir: false
+      emptyOutDir: false,
+      // MARK: disabled `<link rel=modulepreload ...>`
+      // https://github.com/vitejs/vite/pull/9938
+      // https://vitejs.dev/config/build-options.html#build-modulepreload
+      // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel/modulepreload
+      modulePreload: false
     }
   })
 }
