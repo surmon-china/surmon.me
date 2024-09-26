3b2e3deb284f8f691b12652549464bf9e35e5825
Surmon <surmon.me@gmail.com>
2024-09-27 04:14:38 +0700

    feat: build error

--- src/config/bff.yargs.ts
+++ src/config/bff.yargs.ts
@@ -10,4 +10,4 @@ const argv = yargs(process.argv.slice(2)).argv
 export const YOUTUBE_API_KEY = argv.youtube_token
 export const INSTAGRAM_TOKEN = argv.instagram_token
 export const THREADS_TOKEN = argv.threads_token
-export const ZHIHU_COOKIE = '' ?? argv.zhihu_cookie
+export const ZHIHU_COOKIE = argv.zhihu_cookie
