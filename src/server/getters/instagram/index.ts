c2f8927534987bdfe296a9455da1e6a2088f4a9d
Surmon <surmon.me@gmail.com>
2024-12-26 21:57:34 +0800

    fix(bff): Instagram API

--- src/server/getters/instagram/index.ts
+++ src/server/getters/instagram/index.ts
@@ -4,8 +4,11 @@
  * @author Surmon <https://github.com/surmon-china>
  */
 
+// Although the Instagram Basic Display API is no longer in service, the API endpoints continue to be available.
+// https://lightwidget.com/basic-display-api-deprecation
+
 // 1. Generate long-lived access tokens for Instagram Testers (60 days)
-// https://developers.facebook.com/apps/625907498725071/instagram-basic-display/basic-display/
+// https://developers.facebook.com/apps/1308707530127503/instagram-business/API-Setup/?business_id=277570526188879
 
 // 2. Get medias useing API
 // https://developers.facebook.com/docs/instagram-basic-display-api/reference/media#fields
