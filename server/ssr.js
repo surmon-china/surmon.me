61b1c55b366a3b77e154dbc0538878606e65238e
surmon-china <surmon-china@users.noreply.github.com>
2025-03-08 20:20:43 +0000

    deploy: 8f09d38d16f20f89024f1a7d54aaad66589d5f3e

--- server/ssr.js
+++ server/ssr.js
@@ -371,7 +371,7 @@ const languages$1 = [
     data: enLangMap
   }
 ];
-const APP_VERSION = "4.51.1";
+const APP_VERSION = "4.51.3";
 const APP_ENV = "production";
 const isDev = false;
 const isServer = true;
