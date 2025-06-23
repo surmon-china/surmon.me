f975028d5115e7adb761b32cd36174bb83d0bb66
Surmon <surmon.me@gmail.com>
2025-06-23 20:35:26 +0800

    feat: Replaced `yargs` with `yargs-parser`

--- src/config/bff.yargs.ts
+++ src/config/bff.yargs.ts
@@ -4,8 +4,8 @@
  * @author Surmon <https://github.com/surmon-china>
  */
 
-import yargs from 'yargs'
-const argv = yargs(process.argv.slice(2)).argv
+import yargsArgv from 'yargs-parser'
+const argv = yargsArgv(process.argv.slice(2))
 
 export const YOUTUBE_API_KEY = argv.youtube_token
 export const INSTAGRAM_TOKEN = argv.instagram_token
