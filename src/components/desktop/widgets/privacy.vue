d479a76e8375aceb91e9584b6cb51cc318762906
Surmon <surmon.me@gmail.com>
2026-03-14 23:18:36 +0800

    feat: add loading state to privacy modal

--- src/components/desktop/widgets/privacy.vue
+++ src/components/desktop/widgets/privacy.vue
@@ -3,10 +3,12 @@
   import { Markdown } from '/@/effects/markdown'
   import vanilla from '/@/services/vanilla'
 
+  const loading = shallowRef(true)
   const content = shallowRef<string>()
   const fetchPrivacyMarkdown = () => {
     vanilla('/privacy/index.md').then((respnse) => {
       content.value = respnse.data
+      loading.value = false
     })
   }
 
@@ -15,9 +17,18 @@
 
 <template>
   <div class="privacy">
-    <div class="content">
-      <markdown :markdown="content" :compact="true" />
-    </div>
+    <placeholder :loading="loading">
+      <template #loading>
+        <div class="loading">
+          <loading-indicator width="1.8rem" />
+        </div>
+      </template>
+      <template #default>
+        <div class="content">
+          <markdown :markdown="content" :compact="true" />
+        </div>
+      </template>
+    </placeholder>
   </div>
 </template>
 
@@ -26,14 +37,23 @@
   @use '/src/styles/base/functions' as funs;
   @use '/src/styles/base/mixins' as mix;
 
+  $min-height: 20rem;
+
   .privacy {
     width: 46rem;
-    min-height: 18rem;
+    min-height: $min-height;
     max-height: 80vh;
     background-color: $module-bg-opaque;
-    overflow-y: scroll;
+    overflow-y: auto;
     overscroll-behavior-y: none;
     scrollbar-width: thin;
+
+    .loading {
+      width: 100%;
+      height: $min-height;
+      display: flex;
+      justify-content: center;
+    }
 
     .content {
       padding-block: $gap;
